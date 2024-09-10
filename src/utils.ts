eab21aa41f2c30e39515f0e0cd2a26ff5b617e52
Advay Patil <advaypatil27@gmail.com>
2024-09-10 16:55:47 -0700

    Add calculations for one rep max

--- src/utils.ts
+++ src/utils.ts
@@ -1,6 +1,5 @@
 import { ChangeEvent } from "react";
 import * as d3 from "d3";
-import { LineDataPoint } from "./types";
 
 export const handleUpload = (
   e: ChangeEvent<HTMLInputElement>,
@@ -40,4 +39,48 @@ export const handleUpload = (
 
     complete(data);
   };
+};
+
+// Source: strengthlevel.com/one-rep-max-calculator
+const RepetitiontoPercMap: Map<number, number> = {
+  1: 1,
+  2: 0.97,
+  3: 0.94,
+  4: 0.92,
+  5: 0.89,
+  6: 0.86,
+  7: 0.83,
+  8: 0.81,
+  9: 0.78,
+  10: 0.75,
+  11: 0.73,
+  12: 0.71,
+  13: 0.7,
+  14: 0.68,
+  15: 0.67,
+  16: 0.65,
+  17: 0.64,
+  18: 0.63,
+  19: 0.61,
+  20: 0.6,
+  21: 0.59,
+  22: 0.58,
+  23: 0.57,
+  24: 0.56,
+  25: 0.55,
+  26: 0.54,
+  27: 0.53,
+  28: 0.52,
+  29: 0.51,
+  30: 0.5,
+};
+
+const calculateOneRepMax = (weight: number, reps: number) => {
+  const percentage = RepetitiontoPercMap.get(reps);
+
+  if (!percentage) {
+    return -1;
+  }
+
+  return weight / percentage;
 };
