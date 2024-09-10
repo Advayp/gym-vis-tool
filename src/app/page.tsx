e73f42cb4ba7a34447d13a76c417048f4452096a
Advay Patil <advaypatil27@gmail.com>
2024-09-10 16:06:34 -0700

    Transfer line plot date to its own page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,73 +1,7 @@
 "use client";
 
-import { FileUpload } from "@/components/FileUpload";
-import { LinePlot } from "@/components/Visualizations/LinePlotDate";
-import { eliminateDuplicates, extractExerciseNames } from "@/data/utils";
-import { LineDataPoint } from "@/types";
-import { handleUpload } from "@/utils";
-import { Button, Flex, Select } from "@chakra-ui/react";
-import { useEffect, useState } from "react";
+import { Heading } from "@chakra-ui/react";
 
 export default function Home() {
-  const [allData, setAllData] = useState<LineDataPoint[]>([]);
-  const [exerciseList, setExerciseList] = useState<string[]>([]);
-  const [selectedDisplay, setDisplay] = useState<string>("");
-
-  useEffect(() => {
-    setExerciseList(extractExerciseNames(allData));
-    setDisplay(exerciseList[0]);
-  }, [allData]);
-
-  return (
-    <>
-      <Flex justify={"center"} align={"center"} flexDir="column" w="100%">
-        <FileUpload
-          onHandle={(file) => {
-            handleUpload(file, (d) => {
-              setAllData(eliminateDuplicates(d));
-            });
-          }}
-        />
-        {allData.length > 0 && (
-          <>
-            <Select
-              placeholder="Select Exercise"
-              w="25%"
-              defaultValue={exerciseList[0]}
-              onChange={(e) => {
-                setDisplay(e.target.value);
-              }}
-            >
-              {exerciseList.map((e, idx) => {
-                return (
-                  <option value={e} key={idx}>
-                    {e}
-                  </option>
-                );
-              })}
-            </Select>
-            <LinePlot
-              data={allData!.filter((d) => d.name === selectedDisplay)}
-              width={640}
-              height={400}
-              margin={{
-                top: 20,
-                right: 60,
-                bottom: 90,
-                left: 40,
-              }}
-            />
-
-            <Button
-              onClick={() => {
-                console.log(allData.filter((d) => d.name === selectedDisplay));
-              }}
-            >
-              View data
-            </Button>
-          </>
-        )}
-      </Flex>
-    </>
-  );
+  return <Heading>Page</Heading>;
 }
