d92147fb93c015df3643a8d3cf752a15e1ded2cb
Advay Patil <advaypatil27@gmail.com>
2024-09-12 14:56:39 -0700

    Finish 1RM page

--- src/app/onerepmax/page.tsx
+++ src/app/onerepmax/page.tsx
@@ -1,7 +1,80 @@
-import { Heading } from "@chakra-ui/react";
+"use client";
+
+import { FileUpload } from "@/components/FileUpload";
+import { LinePlot } from "@/components/Visualizations/LinePlotDate";
+import {
+  eliminateDuplicates,
+  extractExerciseNames,
+  getOneRepMaxes,
+} from "@/data/utils";
+import { LineDataPoint } from "@/types";
+import { handleUpload } from "@/utils";
+import { Button, Flex, Heading, Select } from "@chakra-ui/react";
+import { useEffect, useState } from "react";
 
 const OneRepMax = () => {
-  return <Heading>1RM</Heading>;
+  const [allData, setAllData] = useState<LineDataPoint[]>([]);
+  const [exerciseList, setExerciseList] = useState<string[]>([]);
+  const [selectedDisplay, setDisplay] = useState("");
+
+  useEffect(() => {
+    setExerciseList(extractExerciseNames(allData));
+    setDisplay(exerciseList[0]);
+  }, [allData]);
+
+  return (
+    <>
+      <Flex w="100%" flexDir={"column"} align={"center"}>
+        <FileUpload
+          onHandle={(file) => {
+            handleUpload(file, (data) => {
+              setAllData(getOneRepMaxes(eliminateDuplicates(data)));
+            });
+          }}
+        />
+        {allData.length > 0 && (
+          <>
+            <Select
+              placeholder="Select Exercise"
+              w="25%"
+              defaultValue={exerciseList[0]}
+              onChange={(e) => {
+                setDisplay(e.target.value);
+              }}
+            >
+              {exerciseList.map((v, k) => {
+                return (
+                  <option value={v} key={k}>
+                    {v}
+                  </option>
+                );
+              })}
+            </Select>
+
+            <LinePlot
+              data={allData!.filter((d) => d.name === selectedDisplay)}
+              width={640}
+              height={400}
+              margin={{
+                top: 20,
+                right: 60,
+                bottom: 90,
+                left: 40,
+              }}
+            />
+
+            <Button
+              onClick={() => {
+                console.log(allData.filter((d) => d.name === selectedDisplay));
+              }}
+            >
+              View data
+            </Button>
+          </>
+        )}
+      </Flex>
+    </>
+  );
 };
 
 export default OneRepMax;
