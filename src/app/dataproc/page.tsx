15d1d6b28c48ab9870e3d1fc6b74b56799048462
Advay Patil <advaypatil27@gmail.com>
2024-09-14 16:34:01 -0700

    Add way of visualizing data

--- src/app/dataproc/page.tsx
+++ src/app/dataproc/page.tsx
@@ -4,7 +4,18 @@ import { FileUpload } from "@/components/FileUpload";
 import { eliminateDuplicates, extractExerciseNames } from "@/data/utils";
 import { DataPoint } from "@/types";
 import { handleUpload } from "@/utils";
-import { Button, Flex } from "@chakra-ui/react";
+import {
+  Button,
+  Flex,
+  Table,
+  TableCaption,
+  TableContainer,
+  Tbody,
+  Td,
+  Th,
+  Thead,
+  Tr,
+} from "@chakra-ui/react";
 import { useEffect, useState } from "react";
 
 const DataProc = () => {
@@ -28,10 +39,38 @@ const DataProc = () => {
         />
         {allData.length > 0 && (
           <>
+            <TableContainer h="50vh" overflowY={"auto"}>
+              <Table variant={"striped"} colorScheme="gray">
+                <Thead>
+                  <Tr>
+                    <Th>Name</Th>
+                    <Th>Date</Th>
+                    <Th>Weight</Th>
+                    <Th>Reps</Th>
+                  </Tr>
+                </Thead>
+                <Tbody>
+                  {allData.map((e, idx) => {
+                    const { name, date, value, reps } = e;
+
+                    return (
+                      <Tr key={idx}>
+                        <Td>{name}</Td>
+                        <Td>{date.toDateString()}</Td>
+                        <Td>{value}</Td>
+                        <Td>{reps}</Td>
+                      </Tr>
+                    );
+                  })}
+                </Tbody>
+              </Table>
+            </TableContainer>
+
             <Button
               onClick={() => {
                 console.log(allData);
               }}
+              mt={5}
             >
               View Data
             </Button>
